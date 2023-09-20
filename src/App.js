d9b24a9697e4dd19b904a800e2b8691d356c1758
Fekadu Tadesse <aaumsccs@gmail.com>
2023-09-20 10:42:25 +0300

    Changes Display Message Additional

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>ይህ የፍቃዱ የመጀመሪያው የሪያክት አፕሎድ ሙከራ የተደረገ ዌብሳይት ነው</h1>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>ይህ የፍቃዱ የመጀመሪያው የሪያክት አፕሎድ የተደረገ ዌብሳይት ነው።</h1>
-        <h1>This is Fekadu deploying testing Online WebSite</h1>
+
+        <h1>This is Fekadu deployment testing Online WebSite</h1>
       </header>
     </div>
   );
